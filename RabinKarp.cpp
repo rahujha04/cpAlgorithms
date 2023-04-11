36a79b5f321a8b8dc24899d3073211ae2d762d42
Rahul Jha <rahujha04@gmail.com>
2023-04-11 19:26:52 +0530

    RabinKarp algorithm added

---
+++ RabinKarp.cpp
@@ -0,0 +1,71 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long 
+const int mod = 1e9 + 7;
+
+
+int find_hash(string &s){
+    int h = 0;
+    for(auto &c: s){
+        h = (h * 1ll * 31 + (c - 'a' + 1))%mod;
+    }
+    return h;
+}
+
+vector<int> rabin_karp(string &s, string &t){
+    vector<int> pos;
+
+    int n = s.size(), m = t.size();
+
+    vector<int> power(max(m, n), 0);
+    
+    int p = 31;
+
+    power[0] = 1;
+
+    for(int i=1;i<power.size();i++){
+        power[i] = (power[i-1] * 1ll * p)%mod;
+    }
+
+    int hash_s = 0;
+
+    for(int i=0;i<n;i++){
+        hash_s = (hash_s + (s[i] - 'a' + 1)* 1ll *power[i])%mod;
+    }
+
+    vector<int> hash_t(m+1, 0);
+
+    for(int i=0;i<m;i++){
+        hash_t[i+1] = (hash_t[i] + (t[i] - 'a' + 1)* 1ll *power[i])%mod;
+    }
+
+    for(int i=0;i<=m-n;i++){
+        int curHash = (hash_t[i+n] - hash_t[i] + mod)%mod;
+
+        if(curHash == (hash_s * 1ll * power[i])%mod){
+            pos.push_back(i);
+        }
+    }
+
+    return pos;
+}
+
+
+
+void solve(){
+    string s, t;
+    cin>>s>>t;
+    vector<int> pos = rabin_karp(s, t);
+    for(int i=0;i<pos.size();i++){
+        cout<<pos[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
